f5ec21cb2416a891d8e2f4d87031c44e5d044fd6
Abstract Programmer <AbstractProgrammer@example.com>
2023-09-29 06:59:28 -0700

    first commit

---
+++ firstc++ program.cpp
@@ -0,0 +1,13 @@
+//Here we are starting with our C++ course.our first program will print hello world
+#include<bits/stdc++.h>    //headerfile
+using namespace std;
+int main(){
+	
+	cout<<"hello world"<<endl;
+	
+	return 0;
+}
+/*A header file in C++ contains declarations and necessary information about 
+functions, variables, and classes that are defined in source 
+files, allowing those source files to access and use these declarations
+*/
